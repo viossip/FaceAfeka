2a588c2d4926894ae69ff90a4d9308484438b20a
Vitaly <viossip@gmail.com>
2017-09-27 13:48:06 +0200

    commit

---
+++ public/js/post_cnt.js
@@ -0,0 +1,26 @@
+function postClicked(event){
+	
+	var postTextElement = $('form textarea[id=postText]');
+	var postText = postTextElement.val();
+
+
+    //event.preventDefault();
+
+	if(!postText || postText.length === 0){
+		userTxt.attr('placeholder', "Write something here...").focus();
+		return;
+    }
+    
+    addPost({}, {}, function name(params) {
+        
+    },
+    function name(params) {
+        
+    });
+}
+
+$(document).ready(function() {
+    //  Attach event handler to the form (sign in button)
+    $('form').submit(postClicked);
+
+});
