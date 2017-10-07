53b848e338cd271e63a2b08c7acecffd219c5e8b
omermyaari <omermyaari@gmail.com>
2017-10-07 19:39:19 +0300

    removed old html files, changed members to friends, added view friends and view photos to user profile

--- routes/friends.js
+++ routes/friends.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('members', { title: 'faceAfeka' });
+  res.render('friends', { title: 'faceAfeka' });
 });
 
 module.exports = router;
